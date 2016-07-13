2fa4538f469bf73f72fa6f28b8ef1a4337ccde6e
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2016-07-13 11:47:28 +0530

    Released version 0.4.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='pyetsy',
-    version='0.4.0',
+    version='0.4.1',
     author='Dan McKinley & Fulfil.IO Inc.',
     author_email='dan@etsy.com,support@fulfil.io',
     description='Python access to the Etsy API',
